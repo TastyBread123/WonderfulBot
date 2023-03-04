c09b96c1de7320fa583a57caf8fbfc6f3712d795
TastyBread123 <117034601+TastyBread123@users.noreply.github.com>
2023-03-04 19:58:12 +0300

    Add files via upload

---
+++ configs/settings.py
@@ -0,0 +1,6 @@
+token = '5500737402:AAFE4wS8L6JUDIaj-ZVFRmaI7_R3zm_k2IA'
+dir = r'C:\!python\проекты\bot_chat'
+version = '2.0'
+
+standart_welcome = "Приветствуем в беседе!"
+tg_channel = 'https://t.me/wonderful_bot_channel'
