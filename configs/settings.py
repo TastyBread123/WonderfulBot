dbb0527cee9484f5f9211ed24010822bf6c127a9
TastyBread123 <117034601+TastyBread123@users.noreply.github.com>
2023-06-28 16:26:39 +0300

    Update settings.py

--- configs/settings.py
+++ configs/settings.py
@@ -1,7 +1,7 @@
-token = '5500737402:AAFE4wS8L6JUDIaj-ZVFRmaI7_R3zm_k2IA'
-dir = r'C:\!python\проекты\bot_chat'
-version = '3.0'
-admins_id = [1215122907]
+token = ''
+dir = r'C:\'
+version = '3.1'
+admins_id = []
 
 standart_welcome = "Приветствуем вас в беседе!"
-tg_channel = 'https://t.me/wonderful_bot_channel'
+tg_channel = 'https://t.me/'
