9cf82b69dac7ac8d611b9d465c8f67fc57369dd6
TastyBread123 <117034601+TastyBread123@users.noreply.github.com>
2023-09-15 16:09:07 +0300

    Add files via upload

--- database.py
+++ database.py
@@ -1,7 +1,5 @@
 import aiosqlite
-
 from configs.settings import standart_welcome
-from utils import get_info
 
 
 async def create_chat_in_db(chat_id: int | str, start_info: tuple):
@@ -12,12 +10,12 @@ async def create_chat_in_db(chat_id: int | str, start_info: tuple):
     :param user_info - Кортеж из следующей информации: id пользователя, username, уровень админки, ник в боте, количество варнов, уровень VIP, общее кол-во exp, кол-во exp необходимые до нового уровня, кол-во exp собранных для нового лвл, уровень
     """
 
-    config_info = (get_info.get_chat_db_id(chat_id), standart_welcome, "Правила еще не установлены!")
+    config_info = (str(chat_id)[1:-1], standart_welcome, "Правила еще не установлены!")
 
     async with aiosqlite.connect('groups.db', check_same_thread=False) as groups_db:
-        await groups_db.execute(f"CREATE TABLE IF NOT EXISTS chat_{get_info.get_chat_db_id(chat_id)}(id INT, login TEXT, admin INT, nick TEXT, warns INT, vip INT, total_exp INT, tolvl_exp INT, need_exp INT, level INT)")
+        await groups_db.execute(f"CREATE TABLE IF NOT EXISTS chat_{str(chat_id)[1:-1]}(id INT, login TEXT, admin INT, nick TEXT, warns INT, vip INT, total_exp INT, tolvl_exp INT, need_exp INT, level INT)")
         await groups_db.execute('CREATE TABLE IF NOT EXISTS config(id INT, welcome TEXT, rules TEXT)')
-        await groups_db.execute(f"INSERT INTO chat_{get_info.get_chat_db_id(chat_id)} (id, login, admin, nick, warns, vip, total_exp, tolvl_exp, need_exp, level) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", (start_info))
+        await groups_db.execute(f"INSERT INTO chat_{str(chat_id)[1:-1]} (id, login, admin, nick, warns, vip, total_exp, tolvl_exp, need_exp, level) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", (start_info))
         await groups_db.execute("INSERT INTO config (id, welcome, rules) VALUES (?, ?, ?)", (config_info))
         await groups_db.commit()
     
@@ -38,7 +36,7 @@ async def add_user(chat_id: int | str, user_info: tuple):
 
     try:
         async with aiosqlite.connect('groups.db', check_same_thread=False) as groups_db:
-            await groups_db.execute(f"INSERT INTO chat_{get_info.get_chat_db_id(chat_id)}(id, login, admin, nick, warns, vip, total_exp, tolvl_exp, need_exp, level) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", (user_info))
+            await groups_db.execute(f"INSERT INTO chat_{str(chat_id)[1:-1]}(id, login, admin, nick, warns, vip, total_exp, tolvl_exp, need_exp, level) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", (user_info))
             await groups_db.commit()
     
     except aiosqlite.OperationalError:
@@ -62,7 +60,7 @@ async def is_user_exists_chat_db(chat_id: int | str, user_id: int | str) -> bool
 
     try:
         async with aiosqlite.connect('groups.db', check_same_thread=False) as groups_db:
-            is_exist = await groups_db.execute(f"SELECT id FROM chat_{get_info.get_chat_db_id(chat_id)} WHERE id = ?", (user_id,))
+            is_exist = await groups_db.execute(f"SELECT id FROM chat_{str(chat_id)[1:-1]} WHERE id = ?", (user_id,))
             is_exist = await is_exist.fetchone()
     
     except aiosqlite.OperationalError:
@@ -85,7 +83,7 @@ async def get_config_data(chat_id: int | str):
 
     try:
         async with aiosqlite.connect('groups.db', check_same_thread=False) as groups_db:
-            data = await groups_db.execute(f"SELECT rules, welcome FROM config WHERE id = '{get_info.get_chat_db_id(chat_id)}'")
+            data = await groups_db.execute(f"SELECT rules, welcome FROM config WHERE id = '{str(chat_id)[1:-1]}'")
             data = await data.fetchone()
     
     except aiosqlite.OperationalError:
@@ -110,7 +108,7 @@ async def set_config_data(chat_id: int | str, column: str, data: int | str):
 
     try:
         async with aiosqlite.connect('groups.db', check_same_thread=False) as groups_db:
-            await groups_db.execute(f"UPDATE config SET {column} = ? WHERE id = '{get_info.get_chat_db_id(chat_id)}'", (data,))
+            await groups_db.execute(f"UPDATE config SET {column} = ? WHERE id = '{str(chat_id)[1:-1]}'", (data,))
             await groups_db.commit()
 
     except aiosqlite.OperationalError:
@@ -134,7 +132,7 @@ async def get_member_chat_info(chat_id: int | str, user_id: int | str):
 
     try:
         async with aiosqlite.connect('groups.db', check_same_thread=False) as groups_db:
-            data = await groups_db.execute(f"SELECT id, login, admin, nick, warns, vip, total_exp, tolvl_exp, need_exp, level FROM chat_{get_info.get_chat_db_id(chat_id)} WHERE id = ?", (user_id,))
+            data = await groups_db.execute(f"SELECT id, login, admin, nick, warns, vip, total_exp, tolvl_exp, need_exp, level FROM chat_{str(chat_id)[1:-1]} WHERE id = ?", (user_id,))
             data = await data.fetchone()
     
     except aiosqlite.OperationalError:
@@ -160,7 +158,7 @@ async def set_member_chat_info(chat_id: int | str, user_id: int | str, column: s
 
     try:
         async with aiosqlite.connect('groups.db', check_same_thread=False) as groups_db:
-            await groups_db.execute(f"UPDATE chat_{get_info.get_chat_db_id(chat_id)} SET {column} = ? WHERE id = ?", (data, user_id))
+            await groups_db.execute(f"UPDATE chat_{str(chat_id)[1:-1]} SET {column} = ? WHERE id = ?", (data, user_id))
             await groups_db.commit()
 
     except aiosqlite.OperationalError:
@@ -184,7 +182,7 @@ async def get_admin_lvl(chat_id: int | str, user_id: int | str):
 
     try:
         async with aiosqlite.connect('groups.db', check_same_thread=False) as groups_db:
-            lvl = await groups_db.execute(f"SELECT admin FROM chat_{get_info.get_chat_db_id(chat_id)} WHERE id = ?", (user_id,))
+            lvl = await groups_db.execute(f"SELECT admin FROM chat_{str(chat_id)[1:-1]} WHERE id = ?", (user_id,))
             lvl = await lvl.fetchone()
     
     except aiosqlite.OperationalError: 
@@ -209,15 +207,15 @@ async def add_message_chat_user(chat_id: int | str, user_id: int | str):
 
     try:
         async with aiosqlite.connect('groups.db', check_same_thread=False) as groups_db:
-            await groups_db.execute(f"UPDATE chat_{get_info.get_chat_db_id(chat_id)} SET total_exp = total_exp + {1}, tolvl_exp = tolvl_exp + {1} WHERE id = ?", (user_id,))
+            await groups_db.execute(f"UPDATE chat_{str(chat_id)[1:-1]} SET total_exp = total_exp + {1}, tolvl_exp = tolvl_exp + {1} WHERE id = ?", (user_id,))
             await groups_db.commit()
-            messages_check = await groups_db.execute(f"SELECT tolvl_exp, need_exp FROM chat_{get_info.get_chat_db_id(chat_id)} WHERE id = ?", (user_id,))
+            messages_check = await groups_db.execute(f"SELECT tolvl_exp, need_exp FROM chat_{str(chat_id)[1:-1]} WHERE id = ?", (user_id,))
             messages_check = await messages_check.fetchone()
             
             if messages_check[0] >= messages_check[1]:
-                await groups_db.execute(f"UPDATE chat_{get_info.get_chat_db_id(chat_id)} SET level = level + {1}, tolvl_exp = {0}, need_exp = need_exp + {100} WHERE id = ?", (user_id,))
+                await groups_db.execute(f"UPDATE chat_{str(chat_id)[1:-1]} SET level = level + {1}, tolvl_exp = {0}, need_exp = need_exp + {100} WHERE id = ?", (user_id,))
                 await groups_db.commit()
-                current_level = await groups_db.execute(f"SELECT level FROM chat_{get_info.get_chat_db_id(chat_id)} WHERE id = ?", (user_id,))
+                current_level = await groups_db.execute(f"SELECT level FROM chat_{str(chat_id)[1:-1]} WHERE id = ?", (user_id,))
                 current_level = await current_level.fetchone()[0]
 
                 return current_level
@@ -244,14 +242,14 @@ async def change_warns(chat_id: int | str, user_id: int | str, action: str):
 
     async with aiosqlite.connect('groups.db', check_same_thread=False) as groups_db:
         if action.startswith('-'):
-            await groups_db.execute(f"UPDATE chat_{get_info.get_chat_db_id(chat_id)} SET warns = warns - {1} WHERE id = ?", (user_id,))
+            await groups_db.execute(f"UPDATE chat_{str(chat_id)[1:-1]} SET warns = warns - {1} WHERE id = ?", (user_id,))
             await groups_db.commit()
         
         elif action.startswith('+'):
-            await groups_db.execute(f"UPDATE chat_{get_info.get_chat_db_id(chat_id)} SET warns = warns + {1} WHERE id = ?", (user_id,))
+            await groups_db.execute(f"UPDATE chat_{str(chat_id)[1:-1]} SET warns = warns + {1} WHERE id = ?", (user_id,))
             await groups_db.commit()
     
-        current_level = await groups_db.execute(f"SELECT warns FROM chat_{get_info.get_chat_db_id(chat_id)} WHERE id = ?", (user_id,))
+        current_level = await groups_db.execute(f"SELECT warns FROM chat_{str(chat_id)[1:-1]} WHERE id = ?", (user_id,))
         current_level = await current_level.fetchone()[0]
     
     return current_level
